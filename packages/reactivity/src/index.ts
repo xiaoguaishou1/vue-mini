ab9ff30dad6c48ddb414009470b24e81172dacd8
monster <760695955@qq.com>
2023-07-31 21:49:50 +0800

    重写

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,2 +1,9 @@
-export { reactive } from './reactive'
-export { effect } from './effect'
+/*
+ * @Author: 阿喜
+ * @Date: 2023-07-31 21:09:16
+ * @LastEditors: 阿喜
+ * @LastEditTime: 2023-07-31 21:41:39
+ * @FilePath: \vue-mini\packages\reactivity\src\index.ts
+ * @Description: 
+ * 
+ */
