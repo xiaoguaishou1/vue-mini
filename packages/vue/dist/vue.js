d42e6ffc79ae91f7ea22710a4d67f09051f1599f
monster <760695955@qq.com>
2023-08-02 00:07:41 +0800

    1
    1

--- packages/vue/dist/vue.js
+++ packages/vue/dist/vue.js
@@ -1,88 +1,184 @@
 var Vue = (function (exports) {
     'use strict';
 
-    //绑定依赖与函数的关系
-    var targetMap = new WeakMap();
     /**
-     * @description: 收集依赖 当依赖被触发时 需要根据依赖的key来获取
-     * @return {*}
+     * 判断是否为一个数组
      */
+    var isArray = Array.isArray;
+    var extend = Object.assign;
+
+    /******************************************************************************
+    Copyright (c) Microsoft Corporation.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+    ***************************************************************************** */
+
+    function __values(o) {
+        var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
+        if (m) return m.call(o);
+        if (o && typeof o.length === "number") return {
+            next: function () {
+                if (o && i >= o.length) o = void 0;
+                return { value: o && o[i++], done: !o };
+            }
+        };
+        throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
+    }
+
+    function __read(o, n) {
+        var m = typeof Symbol === "function" && o[Symbol.iterator];
+        if (!m) return o;
+        var i = m.call(o), r, ar = [], e;
+        try {
+            while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+        }
+        catch (error) { e = { error: error }; }
+        finally {
+            try {
+                if (r && !r.done && (m = i["return"])) m.call(i);
+            }
+            finally { if (e) throw e.error; }
+        }
+        return ar;
+    }
+
+    function __spreadArray(to, from, pack) {
+        if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+            if (ar || !(i in from)) {
+                if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+                ar[i] = from[i];
+            }
+        }
+        return to.concat(ar || Array.prototype.slice.call(from));
+    }
+
+    var createDep = function (effects) {
+        var dep = new Set(effects);
+        return dep;
+    };
+
+    var targetMap = new WeakMap();
     function track(target, key) {
-        //如果当前没有正在执行的effect 则不需要收集依赖
+        // 如果当前不存在执行函数，则直接 return
         if (!activeEffect)
             return;
-        //获取当前target的依赖集合
+        // 尝试从 targetMap 中，根据 target 获取 map
         var depsMap = targetMap.get(target);
-        //如果当前target没有依赖集合 则创建一个
+        // 如果 map 不存在，则生成新的 map，并把该对象赋值给对应的 value
         if (!depsMap) {
             targetMap.set(target, (depsMap = new Map()));
         }
-        //获取当前key的依赖集合
+        // 获取指定 key 的 dep
         var dep = depsMap.get(key);
+        // 如果 dep 不存在，则生成新的 dep，并把该对象赋值给对应的 value
         if (!dep) {
-            depsMap.set(key, (dep = new Set()));
+            depsMap.set(key, (dep = createDep()));
         }
-        //添加依赖
+        trackEffects(dep);
+    }
+    function trackEffects(dep) {
+        // activeEffect
         dep.add(activeEffect);
     }
-    /**
-     * @description: 依赖触发
-     * @param {*} target 代理对象的key 当依赖被触发时 需要根据依赖的key来获取
-     * @param {*} newValue 指定key的最新值
-     * @param {*} oldValue 指定key的旧值
-     * @return {*}
-     */
-    function trigger(target, key, newValue) {
+    function trigger(target, key) {
+        // 获取 target 对应的 map
         var depsMap = targetMap.get(target);
-        if (!depsMap)
+        // 如果 map 不存在，则直接 return
+        if (!depsMap) {
             return;
-        //获取当前key的依赖集合
+        }
+        // 如果 key 不存在，则遍历 map，触发所有的 effect
         var dep = depsMap.get(key);
-        if (!dep)
+        if (!dep) {
             return;
-        //遍历依赖集合 执行依赖
-        dep.forEach(function (effect) {
+        }
+        triggerEffects(dep);
+    }
+    //以此触发依赖
+    function triggerEffects(dep) {
+        var e_1, _a, e_2, _b;
+        var effects = isArray(dep) ? dep : __spreadArray([], __read(dep), false);
+        try {
+            for (var effects_1 = __values(effects), effects_1_1 = effects_1.next(); !effects_1_1.done; effects_1_1 = effects_1.next()) {
+                var effect_1 = effects_1_1.value;
+                if (effect_1.computed) {
+                    triggerEffect(effect_1);
+                }
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (effects_1_1 && !effects_1_1.done && (_a = effects_1.return)) _a.call(effects_1);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+        try {
+            for (var effects_2 = __values(effects), effects_2_1 = effects_2.next(); !effects_2_1.done; effects_2_1 = effects_2.next()) {
+                var effect_2 = effects_2_1.value;
+                if (!effect_2.computed) {
+                    triggerEffect(effect_2);
+                }
+            }
+        }
+        catch (e_2_1) { e_2 = { error: e_2_1 }; }
+        finally {
+            try {
+                if (effects_2_1 && !effects_2_1.done && (_b = effects_2.return)) _b.call(effects_2);
+            }
+            finally { if (e_2) throw e_2.error; }
+        }
+    }
+    //触发 effect
+    function triggerEffect(effect) {
+        if (effect.scheduler) {
+            effect.scheduler();
+        }
+        else {
             effect.run();
-        });
+        }
     }
-    /**
-     * @description: 以ReactEffect实例为单位执行fn
-     * @param {function} fn
-     * @return {*}
-     */
-    function effect(fn) {
-        //生成ReactEffect实例
-        var _effect = new ReactiveEffect(fn);
-        //执行run方法
-        _effect.run();
-    }
-    // 保存当前正在执行的effect
+    //当前的 effect
     var activeEffect;
-    /**
-     * @description: 响应性触发依赖时的执行类
-     * @param {*} any
-     * @return {*}
-     */
     var ReactiveEffect = /** @class */ (function () {
-        function ReactiveEffect(fn) {
+        function ReactiveEffect(fn, scheduler) {
+            if (scheduler === void 0) { scheduler = null; }
             this.fn = fn;
-            this.fn = fn;
+            this.scheduler = scheduler;
         }
         ReactiveEffect.prototype.run = function () {
-            // 保存当前正在执行的effect
             activeEffect = this;
-            // 执行fn
             return this.fn();
         };
+        ReactiveEffect.prototype.stop = function () { };
         return ReactiveEffect;
     }());
+    function effect(fn, options) {
+        // 生成 ReactiveEffect 
+        var _effect = new ReactiveEffect(fn);
+        if (options) {
+            extend(_effect, options);
+        }
+        if (!options || !options.lazy) {
+            _effect.run();
+        }
+    }
 
     /*
      * @Author: 阿喜
      * @Date: 2023-02-10 14:16:04
      * @LastEditors: 阿喜
-     * @LastEditTime: 2023-02-10 15:33:26
-     * @FilePath: /vue3-mini-core/packages/reactivity/src/baseHandlers.ts
+     * @LastEditTime: 2023-02-13 19:58:19
+     * @FilePath: \vue3-core\packages\reactivity\src\baseHandlers.ts
      * @Description: 实现框架中的createGetter  createSetter
      *
      */
@@ -96,7 +192,7 @@ var Vue = (function (exports) {
         get: get,
         set: set
     };
-    function createGetter(isReadonly, shallow) {
+    function createGetter() {
         return function get(target, key, receiver) {
             //利用Reflect.get方法获取值
             var res = Reflect.get(target, key, receiver);
